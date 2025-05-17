b11f5efa7531b8f471c1881b50de15d144b955a1
vlad <vlad.calcea@gmail.com>
2025-05-17 20:42:39 +0300

    Implement admin dashboard, user access control, IsAdmin migration

---
+++ BigBox_v4/Views/Users/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BigBox_v4.Views.Users
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
